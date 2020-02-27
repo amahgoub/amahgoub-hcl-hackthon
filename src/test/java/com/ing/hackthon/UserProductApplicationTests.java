1516c50e6d0a0117840505fd4306d1e67d915573
amahgoub <ahmed.saleh.mahgoub@gmail.com>
2020-02-27 12:10:08 +0100

    project intial upload commit

---
+++ src/test/java/com/ing/hackthon/UserProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.hackthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
